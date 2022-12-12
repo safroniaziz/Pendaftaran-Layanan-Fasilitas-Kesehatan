346d58222fb4ff07e1cfeeb44f50bfc10762be2b
agoyunib <agoyunib@gmail.com>
2022-12-12 11:02:29 +0700

    model pendaftar

--- app/Models/Pendaftar.php
+++ app/Models/Pendaftar.php
@@ -8,9 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Pendaftar extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-=======
-
->>>>>>> 4c71dee6fc4825acb3ecd62315790acbacb528e3
     protected $guarded = [];
 }
